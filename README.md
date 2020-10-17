6f1bcd1e8960ab9312351801c12c8ab04775d7e7
Eros <george.marr.cobc@gmail.com>
2020-10-17 22:28:00 +0100

    add in readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Setup
+
+You will need FFmpeg installed in order for this to run
+see here to setup: https://ffmpeg.org/
+
+### Python requirements
+
+``pip3 install argparse``
+
+# Usage
+
+`python3 main.py -file <fileName> -start <startTime> -end <endTime> -extract -newname -framesstart -framesend -framesall`
+
+additionally can run `-python3 main.py -help`
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     parser.add_argument('extract', type=bool, help='Extract audio into a separate file')
     parser.add_argument('file', type=str, help='Specify the video file')
     parser.add_argument('newname', type=str, help='New name for edited file')
+    parser.add_argument('framesstart', type=str, help='Set where to start converting frames')
+    parser.add_argument('framesend', type=str, help='Set where to end converting frames')
+    parser.add_argument('framesall', type=bool, help='Convert entire video to images')
     args = parser.parse_args()
 
     if not is_video(args.file):
@@ -18,13 +21,29 @@ def main():
     else:
         if args.extract is not None and args.file is not None:
             command = 'ffmpeg -i ' + args.file + '-ab 160k -ac 2 -ar 44100 -vn ' + args.newname + '.wav'
+            print('[+] Extracting audio')
             subprocess.call(command)
         elif args.start is not None and args.end is not None and args.file is not None:
             command = 'ffmpeg -ss ' + args.start + ' -i ' + args.file + ' -t ' + args.end + ' -c copy ' + args.newname + '.mp4'
+            print('[+] Cutting video between ' + args.start + ' ' + args.end)
             subprocess.call(command)
-        
+        elif args.framesstart is not None and args.framesend is not None and args.file is not None:
+            command = 'ffmpeg -i ' + args.file + ' -vf select=`between(t,' + args.framesstart + ',' + args.framesend + ')` -vsync 0 ' + args.newname + '%d.png '
+            print('[+] Converting frames starting at ' + args.framesstart + ' ending at ' + args.framesend)
+            subprocess.call(command)
+        elif args.framesall is True and args.file is not None:
+            command = 'ffmpeg -i ' + args.file + ' -vf fps=1 ' + args.newname + '%d.png'
+            print('[+] Converting all frames to images...')
+            subprocess.call(command)
+        else:
+            print('[!] Something went wrong, check the command!')
+
 
 def is_video(filepath):
     suffix = pathlib.Path(filepath).suffix
     if suffix == 'mp4' or suffix == 'avi' or suffix == 'mov' or suffix == 'wmv' or suffix == 'flv':
         return True
+
+
+if __name__ == '__main__':
+    main()
